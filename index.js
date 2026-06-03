7bd9f2fe7c7c259027176cc1962e30826fb191dc
Felipe Garcia Fogal <felipefogal@gmail.com>
2026-06-03 16:55:27 -0300

    Adicionando calculo de adicional noturno

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-readline.question("Digite o número de horas trabalhadas: \n", (horas) => {
-  readline.question("Digite o valor da hora trabalhada: \n", (valorHora) => {
-    const salarioBase = ((parseFloat(horas) * parseFloat(valorHora) * 4.5) * 1.3);
-    console.log(`Salário base sem DSR: ${salarioBase}`);
-    const salarioBaseMaisDSR = parseFloat(salarioBase / 6);
-    const salarioBruto = salarioBase + salarioBaseMaisDSR;
-    console.log(`O salário bruto é: ${salarioBruto}`);
-    readline.close();
-  });
-});
+readline.question(
+  "Digite o número de horas trabalhadas na semana: \n",
+  (horas) => {
+    readline.question("Digite o valor da hora trabalhada: \n", (valorHora) => {
+      const salarioBase = parseFloat(horas) * parseFloat(valorHora) * 4.5 * 1.3;
+      console.log(`Salário base sem DSR: ${salarioBase}`);
+      const salarioBaseMaisDSR = parseFloat(salarioBase / 6);
+      const salarioBruto = salarioBase + salarioBaseMaisDSR;
+      console.log(`O salário bruto sem adicional noturno é: ${salarioBruto}`);
+      readline.question(
+        "Digite o número de horas trabalhadas após as 22:00 na semana: \n",
+        (horasNoturnas) => {
+          if (parseFloat(horasNoturnas) === 0) {
+            console.log(
+              `Não há horas noturnas, o salário bruto é: ${salarioBruto}`,
+            );
+            readline.close();
+            return;
+          }
+          const adicionalNoturno =
+            parseFloat(horasNoturnas) * parseFloat(valorHora) * 0.2 * 4.5;
+          const salarioBrutoComAdicionalNoturno =
+            salarioBruto + adicionalNoturno;
+          console.log(
+            `O salário bruto com adicional noturno é: ${salarioBrutoComAdicionalNoturno}`,
+          );
+          readline.close();
+        },
+      );
+    });
+  },
+);
